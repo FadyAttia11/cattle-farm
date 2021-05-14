2c39b0d6cec089357c29da4bc130f1434712bc11
FadyAttia11 <fadyattia11@gmail.com>
2021-05-14 22:49:13 +0200

    added admin empty pages & finished product-reserv

--- index-admin.php
+++ index-admin.php
@@ -52,27 +52,11 @@ session_start();
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="index.php">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
+          <li><a href="products-reserv.php">Products Reservations</a></li>
+          <li><a href="new-fodder.php">Add New Fodder</a></li>
+          <li><a href="avail-fodders.php">Available Fodders</a></li>
+          <li><a href="fodders-sales.php">Fodders Sales</a></li>
+          <li><a href="#">Balance: <?php echo $user_data['balance'] ?> L.E</a></li>
           <li><a href="logout.php">Logout</a></li>
 
         </ul>
